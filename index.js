7928192801c94108475056993082ca84d9f2a935
Jonathan Eiten <jonathan.eiten@mojotouch.com>
2017-12-05 11:24:18 -0500

    made version optional; accommodate json

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 /**
- * Created by joneit on 11/28/17.
+ * Create wrapper strings.
+ * @param {string} name
+ * @param {string} [version]
+ * @constructor
  */
+function Wrapper(name, version) {
 
-module.exports = function(name, version) {
-    
-    this.header = '// ' + name + '@' + version + '\n\n' +
-            '(function(require, module, exports) {\n\n';
+    this.header = '(function(require, module, exports) { // ' + name + (version === undefined ? '' : '@' + version) + '\n\n';
+
+    if (/-json$/.test(name)) {
+        this.header += 'module.exports = ';
+    }
 
     this.footer = '\n\n' +
-            '})(fin.Hypergrid.require, fin.Hypergrid.modules, fin.Hypergrid.modules.exports = {});\n\n' +
-            'fin.Hypergrid.modules.exports.\$\$VERSION = \'' + version + '\';\n' +
-            'fin.Hypergrid.modules[\'' + name + '\'] = fin.Hypergrid.modules.exports;\n' +
-            'delete fin.Hypergrid.modules.exports;\n\n';
+        '})(fin.Hypergrid.require, fin.Hypergrid.modules, fin.Hypergrid.modules.exports = {});\n' +
+        (version === undefined ? '' : 'fin.Hypergrid.modules.exports.\$\$VERSION = \'' + version + '\';\n') +
+        'fin.Hypergrid.modules[\'' + name + '\'] = fin.Hypergrid.modules.exports;\n' +
+        'delete fin.Hypergrid.modules.exports;\n\n';
 
-};
+}
+
+module.exports = Wrapper;
