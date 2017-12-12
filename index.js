35a7b60089664c290cbc9809d5ed3382e4117c78
Jonathan Eiten <jonathan.eiten@mojotouch.com>
2017-12-12 07:33:07 -0500

    added Wrapper.prototype.wrap

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 /**
  * Create wrapper strings.
- * @param {string} name
- * @param {string} [version]
+ * @param {string} name - module name
+ * @param {string} version - Merged into standard header and footer.
+ * @param {object} [options]
+ * @param {boolean} [options.isJSON] - If truthy, wraps contents in `module.exports=` and `;`.
+ * @param {string} [options.header] - Custom header (typically paired with a custom footer).
+ * @param {string} [options.footer] - Custom footer (typically paired with a custom header).
  * @constructor
  */
-function Wrapper(name, version) {
+function Wrapper(name, version, options) {
 
-    this.header = '(function(require, module, exports) { // ' + name + (version === undefined ? '' : '@' + version) + '\n\n';
+    options = options || {};
 
-    if (/-json$/.test(name)) {
-        this.header += 'module.exports = ';
-    }
+    var header = options.header ||
+        '(function(require, module, exports) { // ${name}@${version}\n\n' +
+        (options.isJSON ? 'module.exports = ' : '');
 
-    this.footer = '\n\n' +
-        '})(fin.Hypergrid.require, fin.Hypergrid.modules, fin.Hypergrid.modules.exports = {});\n' +
-        (version === undefined ? '' : 'fin.Hypergrid.modules.exports.\$\$VERSION = \'' + version + '\';\n') +
-        'fin.Hypergrid.modules[\'' + name + '\'] = fin.Hypergrid.modules.exports;\n' +
+    var footer = options.footer ||
+        (options.isJSON ? ';' : '') +
+        '\n\n})(fin.Hypergrid.require, fin.Hypergrid.modules, fin.Hypergrid.modules.exports = {});\n' +
+        'fin.Hypergrid.modules.exports.\$\$VERSION = \'${version}\';\n' +
+        'fin.Hypergrid.modules[\'${name}\'] = fin.Hypergrid.modules.exports;\n' +
         'delete fin.Hypergrid.modules.exports;\n\n';
 
+    this.header = merge(header);
+    this.footer = merge(footer);
+
+    function merge(s) {
+        return s.replace(/\$\{(\w+)\}/g, function(match, p1) { return eval(p1); });
+    }
 }
+
+
+Wrapper.prototype.wrap = function(code) {
+    return this.header + code + this.footer;
+};
+
 
 module.exports = Wrapper;
