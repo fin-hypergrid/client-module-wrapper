961f2fe1d037803f1647fa1c5dd9774e71de14ee
Jonathan Eiten <jonathan.eiten@mojotouch.com>
2017-12-14 15:56:22 -0500

    single mansifest param

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 /**
  * Create wrapper strings.
- * @param {string} name - module name
- * @param {string} version - Merged into standard header and footer.
- * @param {object} [options]
- * @param {boolean} [options.isJSON] - If truthy, wraps contents in `module.exports=` and `;`.
- * @param {string} [options.header] - Custom header (typically paired with a custom footer).
- * @param {string} [options.footer] - Custom footer (typically paired with a custom header).
+ * @param {object} manifest
+ * @param {string} manifest.name - module name
+ * @param {string} manifest.version - Merged into standard header and footer.
+ * @param {boolean} [manifest.isJSON] - If truthy, wraps contents in `module.exports=` and `;`.
+ * @param {string} [manifest.header] - Custom header (typically paired with a custom footer).
+ * @param {string} [manifest.footer] - Custom footer (typically paired with a custom header).
  * @constructor
  */
-function Wrapper(name, version, options) {
+function Wrapper(manifest) {
 
-    options = options || {};
-
-    var header = options.header ||
+    var header = manifest.header ||
         '(function(require, module, exports) { // ${name}@${version}\n\n' +
-        (options.isJSON ? 'module.exports = ' : '');
+        (manifest.isJSON ? 'module.exports = ' : '');
 
-    var footer = options.footer ||
-        (options.isJSON ? ';' : '') +
+    var footer = manifest.footer ||
+        (manifest.isJSON ? ';' : '') +
         '\n\n})(fin.Hypergrid.require, fin.Hypergrid.modules, fin.Hypergrid.modules.exports = {});\n' +
         'fin.Hypergrid.modules.exports.\$\$VERSION = \'${version}\';\n' +
         'fin.Hypergrid.modules[\'${name}\'] = fin.Hypergrid.modules.exports;\n' +
@@ -27,7 +25,12 @@ function Wrapper(name, version, options) {
     this.footer = merge(footer);
 
     function merge(s) {
-        return s.replace(/\$\{(\w+)\}/g, function(match, p1) { return eval(p1); });
+        return s.replace(/\$\{(\w+)\}/g, function(match, p1) {
+            if (!(p1 in manifest)) {
+                throw new Error('Expected manifest.' + p1 +' to be defined.');
+            }
+            return manifest[p1];
+        });
     }
 }
 
